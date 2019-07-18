e9a334e333e752b3620d8fbffd0ec74ab650d92e
Christiana Kobiah <christianakobea@gmail.com>
2019-07-18 13:05:56 +0100

    Another one

--- myapp.py
+++ myapp.py
@@ -1,5 +1,7 @@
 
 from flask import *
+import json
+import os
  
 app = Flask(__name__)
 
@@ -27,3 +29,5 @@ def add_todo():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0' ,port=port)
