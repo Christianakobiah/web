5e7b8d748d09cfee024fca126bfad872b5896c7a
Christiana Kobiah <christianakobea@gmail.com>
2019-07-18 15:11:47 +0100

    Have seen it

--- myapp.py
+++ myapp.py
@@ -28,6 +28,6 @@ def add_todo():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    
     port = os.environ.get('PORT', 5000)
     app.run(debug=True, host='0.0.0.0' ,port=port)
