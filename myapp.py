b5dd6cb4de8a5cb03a145025682bd296bb325932
Christiana Kobiah <christianakobea@gmail.com>
2019-07-18 11:52:04 +0100

    I seeyou

---
+++ myapp.py
@@ -0,0 +1,29 @@
+
+from flask import *
+ 
+app = Flask(__name__)
+
+items = []
+''' 
+@app.route('/greetings')
+def index():
+    return 'Hello world'
+
+@app.route('/whereiam')
+def where_i_am():
+    return 'ghana'
+'''
+@app.route('/')
+def tee():
+    return render_template('form.html', items=items)
+
+@app.route('/add_todo')
+def add_todo():
+    item = request.args.get("item")
+    print(item)
+    items.append(item)
+    return redirect("http://localhost:5000/", code = 302)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ pytfunction.py
@@ -0,0 +1,13 @@
+def get_age():
+    a = int(input("What is your age"))
+    print("Your age is :")
+    print(a)
+    
+def name():
+    k = input("May i know your name ")
+    print("My anme is ")
+    print(k)
+    
+name()
+get_age()
+
